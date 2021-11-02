dc0049cb65e4b6b05e66557efa0e78f07c5e8584
Tyler Pashigian <tylerpashigian@gmail.com>
2021-11-02 08:14:04 -0500

    Removing OpenSea calls on a timer and adding event listener using Web3 and ethers libraries

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
-import moment from "moment";
-import * as dotenv from "dotenv";
+import moment from 'moment';
+import Web3 from 'web3';
+import * as dotenv from 'dotenv';
 dotenv.config();
 
-import { getSales } from "./opensea/opensea";
-import { postTweet } from "./twitter-bot/twitter";
-import { Penguin } from "./models/penguin";
+import { getSales } from './opensea/opensea';
+import { postTweet } from './twitter-bot/twitter';
+import { Penguin } from './models/penguin';
+import { request } from './utilities/request';
+import { ethers } from 'ethers';
+
+const ETHERSCAN_ABI_URL = process.env.ETHERSCAN_ENDPOINT || '';
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS || '';
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || '';
 
 let fromTime = moment().unix();
 
@@ -14,7 +21,7 @@ async function main() {
       completedSales.reverse().forEach(async (sale: Penguin, i: number) => {
         const newMoment = moment.utc(sale.timestamp).unix();
         console.log(sale);
-        await postTweet(sale).catch(() => console.log("Error tweeting"));
+        await postTweet(sale).catch(() => console.log('Error tweeting'));
         if (i === completedSales.length - 1) {
           fromTime = newMoment + 1;
         }
@@ -25,4 +32,58 @@ async function main() {
   }
 }
 
-setInterval(main, 10000);
+// setInterval(main, 10000);
+async function getContractAbi() {
+  const abi = await request(
+    `${ETHERSCAN_ABI_URL}${CONTRACT_ADDRESS}&apiKey=${ETHERSCAN_API_KEY}`
+  );
+  // console.log(JSON.parse(abi).result);
+  return JSON.parse(JSON.parse(abi).result);
+}
+
+async function mainNew() {
+  const abi = await getContractAbi();
+
+  const provider = new ethers.providers.WebSocketProvider('wss://mainnet.infura.io/ws/v3/6b4db4e825064a46a1d48a5237a97259');
+  const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, provider);
+
+  contract.on("Transfer", (event) => {
+    console.log(event);
+  });
+
+  // let web3 = new Web3(
+  //   new Web3.providers.WebsocketProvider(
+  //     'wss://mainnet.infura.io/ws/v3/6b4db4e825064a46a1d48a5237a97259'
+  //   )
+  // );
+
+  // const contract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
+
+  // console.log(contract);
+
+  // contract.getPastEvents('Transfer').then((events) => {
+  //   // console.log(events); // same results as the optional callback above
+  //   events.forEach((event) => {
+  //     console.log(event);
+  //   });
+  // });
+
+  // contract.events
+  //   .Transfer({})
+  //   .on('connected', (subscriptionId: any) => {
+  //     console.log(subscriptionId);
+  //   })
+  //   .on('data', (event: any) => {
+  //     console.log(event);
+  //   })
+  //   .on('changed', (event: any) => {
+  //     // remove event from local database
+  //     console.log('changed');
+  //   })
+  //   .on('error', (error: any, receipt: any) => {
+  //     // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
+  //     console.log('error');
+  //   });
+}
+
+mainNew();
---
+++ src/utilities/request.ts
@@ -0,0 +1,22 @@
+import https from 'https';
+
+export function request(url: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    https
+      .get(url, (resp: any) => {
+        let data = '';
+
+        resp.on('data', (chunk: any) => {
+          data += chunk;
+        });
+
+        resp.on('end', () => {
+          resolve(data);
+        });
+      })
+      .on('error', (err: any) => {
+        console.log('Error: ' + err.message);
+        reject(err.message);
+      });
+  });
+}
